ba7b8e1a7a38d92cbcde5a11e468e5f350dc10a7
AutoLS <karston_manho@hotmail.com>
2020-06-28 06:30:33 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -38,41 +38,24 @@ int WinMain(HINSTANCE hInstance,
 	TTF_Font* LabelsFontS = TTF_OpenFont("ast/LibreBaskerville-Regular.ttf", 16);
 	int LabelH = TTF_FontHeight(LabelsFont);
 	
-	label StatusLabelFailed = CreateLabel(V2(), "Cannot find Risk of Rain 2.exe...", 
-										  LabelsFontS, 
-									     {255, 0, 0, 255}, Graphics.Renderer);
-	label StatusLabelSucceed = CreateLabel(V2(), 
-										   "Opened Risk of Rain 2.exe for writing...", 
-										   LabelsFontS, 
-										   {0, 255, 0, 255}, Graphics.Renderer);
+	label StatusLabelFailed = CreateLabel(V2(), "Cannot find Risk of Rain 2.exe...", LabelsFontS, {255, 0, 0, 255}, Graphics.Renderer);
+	label StatusLabelSucceed = CreateLabel(V2(), "Opened Risk of Rain 2.exe for writing...", LabelsFontS, {0, 255, 0, 255}, Graphics.Renderer);
 	
 	image_data* UITextures = LoadUITextures(Graphics.Renderer);
 	
 	v2 GoldPos = V2(100, 50);
-	button GoldButton = CreateButton(GoldPos, V2(50, 50), 
-									 UITextures[0], UITextures[1],
-									 Graphics.Renderer, ButtonFont, "+10k");
-	label GoldLabel = CreateLabel(V2(10, GoldPos.y+25-LabelH*0.5f), "Gold", 
-    							  LabelsFont, {255, 255, 255, 255},
-								  Graphics.Renderer);
+	button GoldButton = CreateButton(GoldPos, V2(50, 50), UITextures[0], UITextures[1], Graphics.Renderer, ButtonFont, "+10k");
+	label GoldLabel = CreateLabel(V2(10, GoldPos.y+25-LabelH*0.5f), "Gold", LabelsFont, {255, 255, 255, 255}, Graphics.Renderer);
 					
 	v2 ULJPos = GoldPos + V2(100, 60);
-	checkbox UnlimitedJumpsCB = CreateCheckbox(ULJPos, V2(38, 36),
-											   UITextures[2], UITextures[3]);
+	checkbox UnlimitedJumpsCB = CreateCheckbox(ULJPos, V2(38, 36), UITextures[2], UITextures[3]);
 											   
-	label UnlimitedJumpsLabel = CreateLabel(V2(10, ULJPos.y+18-LabelH*0.5f), 
-											"Unlimited Jumps", 
-											LabelsFont, {255, 255, 255, 255},
-											Graphics.Renderer);
+	label UnlimitedJumpsLabel = CreateLabel(V2(10, ULJPos.y+18-LabelH*0.5f), "Unlimited Jumps", LabelsFont, {255, 255, 255, 255}, Graphics.Renderer);
 											
 	v2 GodModePos = ULJPos + V2(0, 60);
-	checkbox GodModeCB = CreateCheckbox(GodModePos, V2(38, 36),
-										UITextures[2], UITextures[3]);
+	checkbox GodModeCB = CreateCheckbox(GodModePos, V2(38, 36), UITextures[2], UITextures[3]);
 											   
-	label GodModeLabel = CreateLabel(V2(10, GodModePos.y+18-LabelH*0.5f), 
-									 "God mode", 
-									 LabelsFont, {255, 255, 255, 255},
-									 Graphics.Renderer);
+	label GodModeLabel = CreateLabel(V2(10, GodModePos.y+18-LabelH*0.5f), "God mode", LabelsFont, {255, 255, 255, 255}, Graphics.Renderer);
 							
 	timer AttachProcessTimer = CreateTimer(2.0f);
 	
@@ -118,26 +101,21 @@ int WinMain(HINSTANCE hInstance,
 			uint32 GoldAddrOffset[] = {0x128, 0x760, 0x10, 0x28, 0x58, 0xA0};
 			uint32 GoldModuleOffset = 0x015A95D8;
 
-			uintptr_t GoldAddr = GetDAMAddr(&Attach, ModuleName, GoldModuleOffset,
-											GoldAddrOffset, ArraySize(GoldAddrOffset));
+			uintptr_t GoldAddr = GetDAMAddr(&Attach, ModuleName, GoldModuleOffset, GoldAddrOffset, ArraySize(GoldAddrOffset));
 											 
-			if(ReadProcessMemory(Attach.Process, 
-							 (void*)GoldAddr, &GoldValue, 
-							  sizeof(GoldValue), 0))
+			if(ReadProcessMemory(Attach.Process, (void*)GoldAddr, &GoldValue, sizeof(GoldValue), 0))
 			{
 				if(GoldButton.State == BUTTON_STATE_L_CLICK)
 				{						 
 					GoldValue += 10000;
-					WriteProcessMemory(Attach.Process, (void*)GoldAddr, 
-									   &GoldValue, sizeof(GoldValue), 0);
+					WriteProcessMemory(Attach.Process, (void*)GoldAddr, &GoldValue, sizeof(GoldValue), 0);
 				}
 			}		
 		}
 		
 		char Buffer[100] = {};
 		sprintf(Buffer, "%d", GoldValue);
-		LoadText(Graphics.Renderer, LabelsFont, &GoldValueText, 
-				 Buffer, {255, 0, 0, 255});
+		LoadText(Graphics.Renderer, LabelsFont, &GoldValueText, Buffer, {255, 0, 0, 255});
 				 
 		rect32 TextRect = GoldLabel.Rect;
 		TextRect.Pos += V2(150, 0);
@@ -152,17 +130,13 @@ int WinMain(HINSTANCE hInstance,
 				uint32 JumpModuleOffset = 0x01532288;
 				uint32 JumpAddrOffset[] = 
 				{0x50, 0x188, 0x378, 0x60, 0x30, 0x40, 0x8C};
-				uintptr_t JumpAddr = GetDAMAddr(&Attach, ModuleName, JumpModuleOffset,
-												JumpAddrOffset, ArraySize(JumpAddrOffset));
+				uintptr_t JumpAddr = GetDAMAddr(&Attach, ModuleName, JumpModuleOffset, JumpAddrOffset, ArraySize(JumpAddrOffset));
 				
 				uint32 Value = 0;
 				uint32 Jumps = 0;
 				
-				WriteProcessMemory(Attach.Process, (void*)JumpAddr, 
-								   &Jumps, sizeof(Jumps), 0);
-				ReadProcessMemory(Attach.Process, 
-							 (void*)JumpAddr, &Value, 
-							  sizeof(Value), 0);
+				WriteProcessMemory(Attach.Process, (void*)JumpAddr, &Jumps, sizeof(Jumps), 0);
+				ReadProcessMemory(Attach.Process, (void*)JumpAddr, &Value, sizeof(Value), 0);
 				//printf("JumpAddr: %llx, Value: %d\n", JumpAddr, Value);
 			}
 			
@@ -171,12 +145,10 @@ int WinMain(HINSTANCE hInstance,
 				uint32 ModuleOffset = 0x00491DE8;
 				uint32 AddrOffset[] = {0x490, 0x1B8, 0x60, 0x60, 0x68, 0xA8, 0x68};
 				
-				uintptr_t HPAddr = GetDAMAddr(&Attach, "mono-2.0-bdwgc.dll", ModuleOffset,
-										      AddrOffset, ArraySize(AddrOffset));
+				uintptr_t HPAddr = GetDAMAddr(&Attach, "mono-2.0-bdwgc.dll", ModuleOffset, AddrOffset, ArraySize(AddrOffset));
 
 				real32 Value = 9999;
-				WriteProcessMemory(Attach.Process, (void*)HPAddr, 
-								   &Value, sizeof(Value), 0);			   
+				WriteProcessMemory(Attach.Process, (void*)HPAddr, &Value, sizeof(Value), 0);			   
 			}
 		}
 		
@@ -192,25 +164,20 @@ int WinMain(HINSTANCE hInstance,
 				Sleep(SleepTime);
 			}
 			//assert(Win32GetSecondElapsed(LastCount, SDL_GetPerformanceCounter()) < TargetSecPerFrame);
-			while(Win32GetSecondElapsed(LastCount, SDL_GetPerformanceCounter()) <
-				  Graphics.Display.TargetSecPerFrame);
+			while(Win32GetSecondElapsed(LastCount, SDL_GetPerformanceCounter()) < Graphics.Display.TargetSecPerFrame);
 		}
 		
 		uint64 EndCount = SDL_GetPerformanceCounter();
 		
 		//Render
-		Attach.Status ? RenderLabel(&StatusLabelSucceed, 
-									 Graphics.Renderer) 
-					  : RenderLabel(&StatusLabelFailed, 
-									 Graphics.Renderer);
+		Attach.Status ? RenderLabel(&StatusLabelSucceed, Graphics.Renderer) : RenderLabel(&StatusLabelFailed, Graphics.Renderer);
 		RenderButton(&GoldButton, Graphics.Renderer);
 		RenderLabel(&GoldLabel, Graphics.Renderer);
 		RenderCheckbox(&UnlimitedJumpsCB, Graphics.Renderer);
 		RenderCheckbox(&GodModeCB, Graphics.Renderer);
 		RenderLabel(&UnlimitedJumpsLabel, Graphics.Renderer);
 		RenderLabel(&GodModeLabel, Graphics.Renderer);
-		SDL_RenderCopy(Graphics.Renderer, GoldValueText.Texture,
-					   0, &GoldValueText.Rect);
+		SDL_RenderCopy(Graphics.Renderer, GoldValueText.Texture, 0, &GoldValueText.Rect);
 		SDL_DestroyTexture(GoldValueText.Texture);
 		SDL_RenderPresent(Graphics.Renderer);
 		
